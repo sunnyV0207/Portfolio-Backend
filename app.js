17cf5ddbd1915f775ab9307e76d3971ece11d79a
Sunny Verma <161021054+sunnyV0207@users.noreply.github.com>
2025-05-19 20:58:51 +0530

    changes

--- app.js
+++ app.js
@@ -14,7 +14,11 @@ main()
 })
 
 async function main(){
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URI,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 15000 // increase timeout
+    })
 }
 
 const viewSchema = new mongoose.Schema({
